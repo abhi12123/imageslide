3030a9ef42c97f0331a38e465ad75cbbfe45a0be
abhi12123 <vpofficial55@gmail.com>
2020-11-10 23:31:12 +0530

    image slider finished

---
+++ jsfile.js
@@ -0,0 +1,36 @@
+const slide=document.querySelector('.slide');
+const images=document.querySelectorAll('.slide img')
+
+const prevBtn=document.querySelector('#prevBtn');
+const nxtBtn=document.querySelector('#nxtBtn');
+
+let counter=1;
+const size=images[0].clientWidth;
+
+slide.style.transform='translateX('+(-size*counter)+'px)';
+
+nxtBtn.addEventListener('click',() => {
+    if(counter >= images.length-1) return;
+    slide.style.transition= "transform 0.4s ease-in-out";
+    counter++;
+    slide.style.transform='translateX('+(-size*counter)+'px)';
+});
+
+prevBtn.addEventListener('click',() => {
+    if(counter <= 0) return;
+    slide.style.transition= "transform 0.4s ease-in-out";
+    counter--;
+    slide.style.transform='translateX('+(-size*counter)+'px)';
+});
+slide.addEventListener('transitionend',()=>{
+    if (images[counter].id === 'lastClone'){
+        slide.style.transition= "none";
+        counter=images.length-2;
+        slide.style.transform='translateX('+(-size*counter)+'px)'; 
+    }
+    if (images[counter].id === 'firstClone'){
+        slide.style.transition= "none";
+        counter=images.length-counter;
+        slide.style.transform='translateX('+(-size*counter)+'px)'; 
+    }
+})
